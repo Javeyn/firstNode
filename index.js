4ed58cf478c5bc9d599ed5c8cd67eba82e39e530
Jack Sorensen <jack.sorensen11@gmail.com>
2020-01-05 13:14:47 -0800

    brought over assets from GitLab

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const name = "Tassa";
-// ​
-// //We can 'concatenate' strings together...but this is ugly and will 
-// //const HTMLstring = "<html><body><p>My name is " + name +"</p></body></html>";
-// ​
-// //Another way is to use TEMPLATE LITERALS
-// //Step 1: use BACKTICKS `` to encapsulate the template
-// //Step 2: use ${} to surround variables that you want to swap in!
-const HTMLstring = "<html> <body><p>My name is "+ name + "</p></body></html>";
-// const HTMLstring = "This is a string";
-// ​
+const questions = [
+  
+];
 
-console.log(HTMLstring)
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init()}
