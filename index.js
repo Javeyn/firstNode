8d0257bace994d175d96e17c87e51e74654bd6af
Jack Sorensen <jack.sorensen11@gmail.com>
2020-01-05 13:53:45 -0800

    added NPM calls and .gitignore file

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+const axios = require("axios");
+const generateHTML = require("./generateHTML");
+var pdf = require('html-pdf');
+
+
 const questions = [
   
 ];
